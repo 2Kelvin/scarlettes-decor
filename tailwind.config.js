3dd39e69eb20484bc187b21c3eef6cdbab196717
2Kelvin <rocketmancodes02@gmail.com>
2023-02-17 19:10:12 +0300

    styled upto packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,19 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-        windsong: ["WindSong", ...defaultTheme.fontFamily.serif],
-        poppins: ["Poppins", "sans-serif"],
+        sans: ["Poppins", "Inter", "sans"],
+        windsong: ["WindSong", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+      backgroundImage: {
+        weddings: "url('/resources/images/decor-wedding.jpg')",
+        graduations: "url('/resources/images/decor (17).jpg')",
+        picnics: "url('/resources/images/decor (13).jpg')",
+        proposal: "url('/resources/images/decor-propose.jpg')",
+        babyshower: "url('/resources/images/decor (9).jpg')",
+        anniversary: "url('/resources/images/decor (5).jpg')",
+        birthday: "url('/resources/images/decor (15).jpg')",
+        cooporate: "url('/resources/images/decor (18).jpg')",
       },
     },
   },
